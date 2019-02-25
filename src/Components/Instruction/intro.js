b0c408e700f4a4b27cbff85c4e5ea1cd37d114b5
Joshua <joshuadstarkey@gmail.com>
2019-02-24 20:19:51 -0800

    Getting this deployed via github pages

--- src/Components/Instruction/intro.js
+++ src/Components/Instruction/intro.js
@@ -5,7 +5,7 @@ class Title extends Component {
         return(
             <header className="header">
                 <h1>Sci-Fi Memory Game</h1>
-                <h2>Don't click the same space image twice or it's game over!</h2>
+                <h2>Don't click the same outerspace image twice or it's game over!</h2>
             </header>
         )
     }
